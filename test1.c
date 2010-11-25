3f7a305b8b463a82c0b32d273064a7dd2687bff6
Rohit Nair <roguehit@gmail.com>
2010-11-25 18:53:58 -0500

    Integrated Version

---
+++ test1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <assert.h>
+
+#include "rvm.h"
+#define TEST_STRING "hello, world"
+int main (int argc, char** argv)
+{
+rvm_t rvm;
+trans_t trans;
+rvm = rvm_init("testfolder");
+char* segment[0];
+segment[0] = (char*) rvm_map(rvm,"testsegment",1000);
+
+/* Begin tRansaction */
+ trans = rvm_begin_trans(rvm, 1, (void **) segment);
+ 
+printf("%s\n",rvm->dir);
+sprintf(segment[0],"%s\n","Testing....");
+
+printf("Just Wrote %s",segment[0]);
+//printf("Segment Name:%s\n", rvm->segment[0]->name);
+/*Update Transaction */
+rvm_about_to_modify(trans, segment[0], 0, 10);
+sprintf(segment[0], TEST_STRING);
+
+
+rvm_unmap(rvm,segment[0]);
+//rvm_destroy(rvm,"testsegment");
+
+/*XXX This needs to be in the cleanup function*/
+close(rvm->segment[0]->fd);
+free(rvm->segment[0]);
+
+fclose(rvm->log);
+free(rvm);
+
+return 0;
+}
